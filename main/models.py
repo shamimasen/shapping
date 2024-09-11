53c47b4b1df11210cf160475c436dd2e08463dc0
shamimasen <shalyalionita@gmail.com>
2024-09-11 10:54:41 +0700

    final edit

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class MoodEntry(models.Model):
+    nama = models.CharField(max_length=200)
+    kelas = models.IntegerField()
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
