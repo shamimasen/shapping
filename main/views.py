d60cea8543037dfff44f818d9c6173b248558c40
shamimasen <shalyalionita@gmail.com>
2024-09-11 10:32:11 +0700

    edited

--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
+        'nama' : 'Shalya Naura Lionita',
+        'kelas' : 'PBP B',
         'name' : 'Liquid Blush',
         'price': 'Rp350.000,00',
         'description': 'Meet our new Liquid Blush with high quiality ingredients that will blow your mind!',
